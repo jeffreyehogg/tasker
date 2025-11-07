a5234d2f2b210bce67d896e1f92a93e536577d4e
Jeff Hogg <jeffhogg86@gmail.com>
2025-11-07 10:34:40 -0600

    add entity exports

--- packages/api/src/entry.ts
+++ packages/api/src/entry.ts
@@ -1,3 +1,7 @@
 export { Link } from './links/entities/link.entity';
 export { CreateLinkDto } from './links/dto/create-link.dto';
 export { UpdateLinkDto } from './links/dto/update-link.dto';
+
+export { Board } from './boards/entities/board.entity';
+export { List } from './lists/entities/list.entity';
+export { Task } from './tasks/entities/task.entity';
