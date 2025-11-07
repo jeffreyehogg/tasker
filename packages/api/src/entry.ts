0e1f8e20ff7edc3e7574fd294173a64fd35ea6e0
Jeff Hogg <jeffhogg86@gmail.com>
2025-11-07 10:51:29 -0600

    update entry file

--- packages/api/src/entry.ts
+++ packages/api/src/entry.ts
@@ -1,7 +1,19 @@
+// Existing exports
 export { Link } from './links/entities/link.entity';
 export { CreateLinkDto } from './links/dto/create-link.dto';
 export { UpdateLinkDto } from './links/dto/update-link.dto';
 
+// Board
 export { Board } from './boards/entities/board.entity';
+export { CreateBoardDto } from './boards/dto/create-board.dto';
+export { UpdateBoardDto } from './boards/dto/update-board.dto';
+
+// List
 export { List } from './lists/entities/list.entity';
+export { CreateListDto } from './lists/dto/create-list.dto';
+export { UpdateListDto } from './lists/dto/update-list.dto';
+
+// Task
 export { Task } from './tasks/entities/task.entity';
+export { CreateTaskDto } from './tasks/dto/create-task.dto';
+export { UpdateTaskDto } from './tasks/dto/update-task.dto';
