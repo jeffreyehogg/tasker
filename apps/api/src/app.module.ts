342d2f68d615468434d56c2425ded18e344b3445
Jeff Hogg <jeffhogg86@gmail.com>
2025-11-07 11:29:45 -0600

    add api modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,9 +4,12 @@ import { LinksModule } from './links/links.module';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { BoardsModule } from './boards/boards.module';
+import { ListsModule } from './lists/lists.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [LinksModule],
+  imports: [LinksModule, BoardsModule, ListsModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
